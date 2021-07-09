11a4b35e54ae2ea00552aa03c3e6acb2b4a35d50
Salva <svalladaresr@miumg.edu.gt>
2021-07-09 15:33:54 -0600

    docker

--- src/app/config/utilitis.ts
+++ src/app/config/utilitis.ts
@@ -1,5 +1,5 @@
 export abstract class Urls {
-    static readonly URL_API: string = 'http://localhost:4000/'
+    static readonly URL_API: string = 'http://ec2-35-80-177-188.us-west-2.compute.amazonaws.com:4000/'
     static readonly VEHICULOS: string = 'vehiculos'
     static readonly CLIENTES: string = 'clientes'
     static readonly AGENTES: string = 'agentes'
